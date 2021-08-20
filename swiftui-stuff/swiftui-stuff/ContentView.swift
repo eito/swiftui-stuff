72e88e2da31de6ddf835702efd44a70e08d02cbc
Eric Ito <eric.ito@gmail.com>
2021-08-19 21:50:50 -0700

    initil commit

---
+++ swiftui-stuff/swiftui-stuff/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-stuff
+//
+//  Created by Eric Ito on 8/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-stuff/swiftui-stuff/swiftui_stuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_stuffApp.swift
+//  swiftui-stuff
+//
+//  Created by Eric Ito on 8/19/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_stuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
